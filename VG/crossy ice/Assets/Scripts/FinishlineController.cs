8b464e394ee3f6b6f83927dd291262c250499c6e
coldhed <santiago.rdgz2001@gmail.com>
2023-04-28 19:58:11 -0600

    Added collider with finish line so that player can win

--- VG/crossy ice/Assets/Scripts/FinishlineController.cs
+++ VG/crossy ice/Assets/Scripts/FinishlineController.cs
@@ -1,17 +1,17 @@
 /*
-    Script to controll the player's collision with water, making the player lose
+    Script to controll the player's collision with the finish line, making the player win
 */
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaterController : MonoBehaviour
+public class FinishlineController : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.name.Equals("Player")) {
-            GameMan.Instance.ShowGameOverScreen();
+            GameMan.Instance.ShowGameWinScreen();
             Time.timeScale = 0f;
         }
     }
